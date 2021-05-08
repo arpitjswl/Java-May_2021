768a305d95b4b12fb97792ec2f1be6fbc6e6bc31
arpitjswl <38578792+arpitjswl@users.noreply.github.com>
2021-05-08 22:31:20 +0530

    Add files via upload
    Adding Spring boot integration with Oracle Database on 8th May 2021

---
+++ AngularIntegration/src/main/java/DAO/Student_DAO.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import java.util.List;
+
+import com.arpit.srpingboot.AngularIntegration.model.Student;  
+  
+  
+public interface Student_DAO {  
+  
+    public boolean saveStudent(Student student);  
+    public List<Student> getStudents();  
+    public boolean deleteStudent(Student student);  
+    public List<Student> getStudentByID(Student student);  
+    public boolean updateStudent(Student student);  
+}  
