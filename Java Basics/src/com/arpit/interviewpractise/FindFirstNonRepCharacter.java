8faefe472fdb8f90ed1ba96be110817e46dc1296
Arpit Jaiswal <jswl.arpit@gmail.com>
2021-05-17 15:49:37 +0530

    all

--- Java Basics/src/com/arpit/interviewpractise/FindFirstNonRepCharacter.java
+++ Java Basics/src/com/arpit/interviewpractise/FindFirstNonRepCharacter.java
@@ -1,9 +1,47 @@
 package com.arpit.interviewpractise;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class FindFirstNonRepCharacter {
 
 	public static void main(String[] args) {
-		//adding from 
+		String input = "ABABBBERRRRK";
+		char arr[] = input.toCharArray(); 
+		
+		HashMap<Character, Integer> hm = new HashMap<>();
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (hm.containsKey(arr[i]) == false)
+				hm.put(arr[i], 1);
+			else {
+				int count = hm.get(arr[i]);
+				hm.put(arr[i], ++count);
+			}
+		}
+		
+		System.out.println(hm);
+		
+		char maxKey = ' ';
+		int maxVal = 0;
+		
+		Set<Entry<Character, Integer>> set = hm.entrySet();
+		
+		for (Map.Entry<Character, Integer> map : set) {
+			if (map.getValue() > maxVal) {
+				maxVal = map.getValue();
+				maxKey = map.getKey();
+			}
+			
+			
+			
+		}
+		System.out.println("Character repated most is : " + maxKey + maxVal + " times");
+		System.out.println();
+		
+		
 
 	}
 
